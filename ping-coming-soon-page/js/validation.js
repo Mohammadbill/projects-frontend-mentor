e5af44663587163896061c9a163e29d4b5b33da5
Fraser Watt <fsrwatt@gmail.com>
2021-04-01 22:00:19 +0100

    Removing console log

--- ping-coming-soon-page/js/validation.js
+++ ping-coming-soon-page/js/validation.js
@@ -1,10 +1,10 @@
 const emailRegex = new RegExp("[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}", 'g');
 const changeElements = [...document.getElementsByClassName('validation')];
-console.log(changeElements)
+// console.log(changeElements)
 
 document.getElementById('form-submit').addEventListener('click', () => {
     let emailInput = document.getElementById('email');
-    console.log(emailInput)
+    // console.log(emailInput)
     let email =  String(emailInput.value).toUpperCase();
     
     if (!(emailRegex.test(email))) {
