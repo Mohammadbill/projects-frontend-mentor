660b70e781465a43b57612243183554c13770523
Fraser Watt <fsrwatt@gmail.com>
2021-04-01 21:33:26 +0100

    Sorted validation and correct error message formatting

--- ping-coming-soon-page/js/validation.js
+++ ping-coming-soon-page/js/validation.js
@@ -1,8 +1,10 @@
 const emailRegex = new RegExp("[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}", 'g');
-const changeElements = [...document.getElementsByClassName('validation-pending')];
+const changeElements = [...document.getElementsByClassName('validation')];
+console.log(changeElements)
 
 document.getElementById('form-submit').addEventListener('click', () => {
     let emailInput = document.getElementById('email');
+    console.log(emailInput)
     let email =  String(emailInput.value).toUpperCase();
     
     if (!(emailRegex.test(email))) {
