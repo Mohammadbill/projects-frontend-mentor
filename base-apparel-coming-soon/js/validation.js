f46b4a78d176c28d382bb3fd672a4cb5f6d6627f
Fraser Watt <fsrwatt@gmail.com>
2021-04-25 21:01:58 +0100

    Form validation

--- base-apparel-coming-soon/js/validation.js
+++ base-apparel-coming-soon/js/validation.js
@@ -1,7 +1,22 @@
-
-let validateEmail = (event) => validateInput() ? true : event.preventDefault()
-
 function validateInput() {
     const emailRegex = document.querySelector('input').value.match(/(^[a-z0-9_\.-]{3,})@{1}([a-z0-9]{3,})(\.{1}[a-z]{2,}){1,2}/i)
-    return (emailRegex) ? {'email': emailRegex.input} : false;
+    if (emailRegex) {
+        removeValidationError()
+        return {'email': emailRegex.input}
+    }
+    else {
+        activateValidationError()
+        return false     
+    }
 }
+function activateValidationError () {
+    const errorElements = ['.error-icon', '.error-message', 'form'];
+    errorElements.forEach( elem => document.querySelector(elem).classList.add('active'))
+}
+
+function removeValidationError () {
+    const errorElements = ['.error-icon', '.error-message', 'form'];
+    errorElements.forEach( elem => document.querySelector(elem).classList.remove('active'))
+}
+
+let validateEmail = (event) => validateInput() ? true : event.preventDefault()
